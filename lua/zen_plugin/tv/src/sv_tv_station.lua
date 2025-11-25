bb9ae3d78a3819837d005531733c4bf12a8ac91a
JTaylerJ <jtaylerj@gmail.com>
2025-11-25 14:10:45 -0800

    Remove debug message

--- lua/zen_plugin/tv/src/sv_tv_station.lua
+++ lua/zen_plugin/tv/src/sv_tv_station.lua
@@ -5,8 +5,6 @@ module("zen")
 nt.Receive("tv.sendToTV", {"uint16", "string"}, function(ply, entID, url)
     local ent = Entity(entID)
 
-    print("Received tv.sendToTV for entity:", entID, "with URL:", url)
-
     if not IsValid(ent) or ent:GetClass() ~= "entity_zen_tv" then return end
 
     ent:SetNW2String("tv_url", url)
